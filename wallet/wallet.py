fb5a0c0ba737a29c9badf076870ea4c9655959e4
Andrey <v.andrei.dmitrievich@gmail.com>
2026-03-11 23:13:19 +0300

    decorator added

--- wallet/wallet.py
+++ wallet/wallet.py
@@ -1,8 +1,26 @@
 from dataclasses import dataclass
 from decimal import Decimal
+from typing import Callable
+from functools import wraps
 
 from currency import Currency
 from exceptions import WalletDoesNotFound, WalletLTZero
+
+
+def check_data(func: Callable):
+    @wraps(func)
+    def wrapper(self, currency: Currency, value: Decimal, *args, **kwargs):
+        if self.currency.get(currency) is None:
+            raise WalletDoesNotFound('Ваш кошелёк не поддерживает данную валюту')
+
+        if not isinstance(value, Decimal):
+            raise TypeError('Цена должна быть в Decimal')
+
+        if value <= 0:
+            raise ValueError('Цена должна быть больше 0')
+
+        func(self, currency, value)
+    return wrapper
 
 
 @dataclass
@@ -12,28 +30,12 @@ class Wallet:
     def __post_init__(self):
         self.currency: dict[Currency, Decimal] = {k: Decimal(0) for k in self.currency}
 
+    @check_data
     def replenishment(self, currency: Currency, value: Decimal) -> None:
-        if self.currency.get(currency) is None:
-            raise WalletDoesNotFound('Ваш кошелёк не поддерживает данную валюту')
-
-        if not isinstance(value, Decimal):
-            raise TypeError('Цена должна быть в Decimal')
-
-        if value <= 0:
-            raise ValueError('Цена должна быть больше 0')
-
         self.currency[currency] += value
 
+    @check_data
     def withdraw(self, currency: Currency, value: Decimal) -> None:
-        if self.currency.get(currency) is None:
-            raise WalletDoesNotFound('Ваш кошелёк не поддерживает данную валюту')
-
-        if not isinstance(value, Decimal):
-            raise TypeError('Цена должна быть в Decimal')
-
-        if value <= 0:
-            raise ValueError('Цена должна быть больше 0')
-
         if self.currency[currency] - value < 0:
             raise WalletLTZero('Баланс не может быть меньше 0')
 
