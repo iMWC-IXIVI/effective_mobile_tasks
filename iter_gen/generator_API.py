0ee816b13f1c8ada3a49f0e33fc6e29dffa0b9d9
Andrey <v.andrei.dmitrievich@gmail.com>
2026-03-12 10:45:34 +0300

    added regerator api

---
+++ iter_gen/generator_API.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass, field
+from itertools import batched
+from typing import Iterable, TypeAlias
+
+
+SomeRemoteData: TypeAlias = int
+
+
+@dataclass
+class Query:
+    per_page: int = 3
+    page: int = 1
+
+
+@dataclass
+class Page:
+    per_page: int = 3
+    results: Iterable[SomeRemoteData] = field(default_factory=list)
+    next: int | None = None
+
+
+def request(query: Query) -> Page:
+    data = [i for i in range(0, 10)]
+    chunks = list(batched(data, query.per_page))
+    return Page(
+        per_page=query.per_page,
+        results=chunks[query.page - 1],
+        next=query.page + 1 if query.page < len(chunks) else None,
+    )
+
+
+class GeneratorAPI:
+    def __init__(self, per_page: int):
+        self._per_page = per_page
+        self._page = 1
+
+    def __iter__(self):
+        while True:
+            page = request(Query(per_page=self._per_page, page=self._page))
+
+            yield page
+
+            if page.next is None:
+                break
+
+            self._page = page.next
+
+
+test = GeneratorAPI(per_page=3)
+print(list(test))
