20ba96b0c4b70a88f73f3636f89e2e0ec328e22e
Andrey <v.andrei.dmitrievich@gmail.com>
2026-03-12 09:33:21 +0300

    generator added

--- iter_gen/generator.py
+++ iter_gen/generator.py
@@ -1,14 +1,13 @@
 from exceptions import FibLTZero
 
 
-class Fib:
+class Generator:
     def __init__(self, end):
 
         if end < 0:
             raise FibLTZero('Введённое число не может быть меньше 0')
 
         self._end = end
-        self._index = 0
 
     def _fib(self, n: int):
         if n == 0:
@@ -18,12 +17,5 @@ class Fib:
         return self._fib(n - 1) + self._fib(n - 2)
 
     def __iter__(self):
-        self._index = 0
-        return self
-
-    def __next__(self):
-        if self._index > self._end:
-            raise StopIteration
-
-        self._index += 1
-        return self._fib(self._index - 1)
+        for index in range(self._end + 1):
+            yield self._fib(index)
