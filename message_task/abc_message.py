95faf07e864091f5ecd36aeeba1feca374a3b536
Andrey <v.andrei.dmitrievich@gmail.com>
2026-03-13 15:24:04 +0300

    ref code in messages

--- message_task/abc_message.py
+++ message_task/abc_message.py
@@ -6,13 +6,15 @@ from exceptions import MessageValueError, ParserValueError
 
 
 class TypeMessage(enum.Enum):
-    """Класс Enum для перечисления источников
-    TELEGRAM = enum.auto()
+    """
+    Класс Enum для перечисления источников
     """
     TELEGRAM = enum.auto()
+    MATTERMOST = enum.auto()
+    SLACK = enum.auto()
 
 
-class MainMessage(ABC):
+class JSONMessage(ABC):
     def __init__(self, message_type: TypeMessage, payload: dict):
         """Обязательные атрибуты
         :param message_type: TypeMessage (тип сообщения)
@@ -28,34 +30,72 @@ class MainMessage(ABC):
 
     @abstractmethod
     def to_dict(self) -> dict:
-        """Создаёт унифицированное представление сообщений"""
+        """Возвращение сообщения"""
+        pass
+
+
+class TelegramMessage(JSONMessage):
+    def to_dict(self) -> dict:
+        return self.payload
+
+
+class MattermostMessage(JSONMessage):
+    def __init__(self, payload: dict):
+        super().__init__(TypeMessage.MATTERMOST, payload)
+
+    def to_dict(self) -> dict:
+        return self.payload
+
+
+class SlackMessage(JSONMessage):
+    def __init__(self, payload: dict):
+        super().__init__(TypeMessage.SLACK, payload)
+
+    def to_dict(self) -> dict:
+        return self.payload
+
+
+class ParsedMessage:
+    def __init__(self, type_message: TypeMessage, payload: dict):
+        self.type_message = type_message
+        self.payload = payload
+
+    def to_dict(self):
+        return {
+            'type': self.type_message.name,
+            'payload': self.payload
+        }
 
 
 class Parser(ABC):
-    def __init__(self, type_message: TypeMessage):
-        if not type_message or not isinstance(type_message, TypeMessage):
-            raise ParserValueError('Тип сообщения должен быть не пустым либо иметь тип TypeMessage')
-
-        self.type_message = type_message
-
     @abstractmethod
-    def parser(self) -> dict:
-        """Реализация парсера"""
+    def parse(self, message: JSONMessage) -> ParsedMessage:
+        """Преобразование сообщения в нужный тип сообщения"""
+        pass
 
 
 class TelegramParser(Parser):
-    def __init__(self):
-        super().__init__(TypeMessage.TELEGRAM)
+    def parse(self, message: JSONMessage):
+        return ParsedMessage(TypeMessage.TELEGRAM, message.payload)
 
-    def parser(self):
-        """Реализация парсера telegram"""
+
+class MattermostParser(Parser):
+    def parse(self, message: JSONMessage):
+        return ParsedMessage(TypeMessage.MATTERMOST, message.payload)
+
+
+class SlackParser(Parser):
+    def parse(self, message: JSONMessage):
+        return ParsedMessage(TypeMessage.SLACK, message.payload)
 
 
 class FactoryParser:
     def create_parser(self, message_type: TypeMessage) -> Parser:
         """Создание фабрики парсера"""
         parsers = {
-            TypeMessage.TELEGRAM: TelegramParser
+            TypeMessage.TELEGRAM: TelegramParser,
+            TypeMessage.MATTERMOST: MattermostParser,
+            TypeMessage.SLACK: SlackParser
         }
         parser = parsers.get(message_type)
 
