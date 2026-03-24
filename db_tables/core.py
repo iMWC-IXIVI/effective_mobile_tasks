9b57bbc2aa6d00d535635214967c7b39f1e303d1
Andrey <v.andrei.dmitrievich@gmail.com>
2026-03-24 12:17:57 +0300

    added core for db

---
+++ db_tables/core.py
@@ -0,0 +1,18 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+DB_URL = os.getenv('DB_URL')
+engine = create_engine(url=DB_URL)
+Session = sessionmaker(engine)
+
+
+class Base(DeclarativeBase):
+    pass
