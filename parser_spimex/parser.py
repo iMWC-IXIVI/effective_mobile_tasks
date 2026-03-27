dfa5fb00fc0b0fad8b8c98c0f4c7fc47340dfed7
Andrey <v.andrei.dmitrievich@gmail.com>
2026-03-27 14:01:29 +0300

    added parse site

---
+++ parser_spimex/parser.py
@@ -0,0 +1,36 @@
+import bs4 as bs
+
+from urllib import request
+from pathlib import Path
+
+
+BASE_URL = 'https://spimex.com/'
+URL = 'https://spimex.com/markets/oil_products/trades/results/'
+SAVE_PATH = Path('./files')
+
+
+def get_html() -> bs.BeautifulSoup:
+    """Получение HTML страницы"""
+    response = request.urlopen(url=URL)
+    parse = bs.BeautifulSoup(response.read(), 'html.parser')
+    response.close()
+
+    return parse
+
+
+def parse_html(parse: bs.BeautifulSoup) -> tuple[str, str]:
+    """Получение ссылки для скачивания + имя файла"""
+    div_inner_item = parse.find('div', class_='accordeon-inner__item')
+
+    a_link = div_inner_item.find('a', class_='accordeon-inner__item-title link pdf')
+    p_date = div_inner_item.find('p')
+
+    file_name = p_date.find('span').text + '.pdf'
+    download_url = BASE_URL + a_link.get('href')
+
+    return download_url, file_name
+
+
+def download_file(url: str, file_path: str) -> None:
+    """Загрузка файла"""
+    request.urlretrieve(url, SAVE_PATH/file_path)
