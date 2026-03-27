487bd871fa298e78fe39afb642b1b942cd9c77d9
Andrey <v.andrei.dmitrievich@gmail.com>
2026-03-27 20:41:58 +0300

    added all files

--- parser_spimex/parser.py
+++ parser_spimex/parser.py
@@ -1,5 +1,6 @@
 import bs4 as bs
 
+from datetime import datetime
 from urllib import request
 
 from core import SRC_PATH
@@ -7,30 +8,71 @@ from core import SRC_PATH
 
 BASE_URL = 'https://spimex.com/'
 URL = 'https://spimex.com/markets/oil_products/trades/results/'
+BASE_DATE = datetime.strptime('01.01.2023', '%d.%m.%Y').date()
 
 
-def get_html() -> bs.BeautifulSoup:
+def get_html(url: str) -> bs.BeautifulSoup:
     """Получение HTML страницы"""
-    response = request.urlopen(url=URL)
+    response = request.urlopen(url=url)
     parse = bs.BeautifulSoup(response.read(), 'html.parser')
     response.close()
 
     return parse
 
 
-def parse_html(parse: bs.BeautifulSoup) -> tuple[str, str]:
+def parse_html(parse: bs.BeautifulSoup) -> list[tuple[str, str]] | tuple[list[tuple[str, str]], str]:
     """Получение ссылки для скачивания + имя файла"""
-    div_inner_item = parse.find('div', class_='accordeon-inner__item')
+    div_inner_item_lst = None
+    result = []
 
-    a_link = div_inner_item.find('a', class_='accordeon-inner__item-title link pdf')
-    p_date = div_inner_item.find('p')
+    main_div = parse.find_all('div', class_='page-content__tabs__block')
+    for div in main_div:
+        if div.get('data-tabcontent') == '1':
+            div_inner_item_lst = div.find_all('div', class_='accordeon-inner__wrap-item')
 
-    file_name = p_date.find('span').text + '.pdf'
-    download_url = BASE_URL + a_link.get('href')
+    if div_inner_item_lst is None:
+        return result
 
-    return download_url, file_name
+    for div_inner_item in div_inner_item_lst:
+        a_link = div_inner_item.find('a', class_='accordeon-inner__item-title link pdf')
+
+        if a_link is None:
+            a_link = div_inner_item.find('a', class_='accordeon-inner__item-title link xls')
+            prefix = '.xls'
+        else:
+            prefix = '.pdf'
+
+        p_date = div_inner_item.find('p')
+        file_date = p_date.find('span').text
+
+        file_dt = datetime.strptime(file_date, '%d.%m.%Y').date()
+        if file_dt <= BASE_DATE:
+            return result
+
+        file_name = file_date + prefix
+        download_url = BASE_URL + a_link.get('href')
+
+        result.append((download_url, file_name))
+
+    li_next_page_link = parse.find('li', class_='bx-pag-next')
+    a_li_next_page_link = li_next_page_link.find('a')
+    next_page = a_li_next_page_link.get('href')
+
+    return result, BASE_URL + next_page
 
 
-def download_file(url: str, file_path: str) -> None:
+def download_file(lst_urls: list[tuple[str, str]]) -> None:
     """Загрузка файла"""
-    request.urlretrieve(url, SRC_PATH/file_path)
+    for url, file_path in lst_urls:
+        request.urlretrieve(url, SRC_PATH/file_path)
+
+
+def main(url: str):
+    parse = get_html(url)
+    urls = parse_html(parse)
+
+    if isinstance(urls, tuple):
+        download_file(urls[0])
+        main(urls[1])
+    elif isinstance(urls, list):
+        download_file(urls)
