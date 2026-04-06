5416bc94141ba4d5ccaf17bef17a97a8e9f4a428
Andrey <v.andrei.dmitrievich@gmail.com>
2026-04-06 18:25:35 +0300

    add: video app

--- video_platform/video_platform/settings.py
+++ video_platform/video_platform/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # APPS
-
+    'video_app.apps.VideoAppConfig'
     # INSTALLED APPS
 
 ]
--- video_platform/video_platform/urls.py
+++ video_platform/video_platform/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('v1/video/', include('video_app.urls'))
 ]
