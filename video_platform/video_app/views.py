3dd1f3066739905b93a4a99eff9682a9095fcd63
Andrey <v.andrei.dmitrievich@gmail.com>
2026-04-07 14:26:30 +0300

    add: action likes

--- video_platform/video_app/views.py
+++ video_platform/video_app/views.py
@@ -1,7 +1,12 @@
+from django.db import transaction
+from django.db.models import F
+
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 
-from .models import Video
+from .models import Video, Like
 from .serializers import VideoModelSerializer, VideoDetailModelSerializer
 from .permissions import PublishedOrOwnerPermission
 
@@ -20,3 +25,20 @@ class VideoViewSet(ReadOnlyModelViewSet):
             serialize = self.get_serializer(video)
 
         return Response(serialize.data)
+
+    @action(detail=True, methods=['post'])
+    @transaction.atomic
+    def likes(self, request, pk, *args, **kwargs):
+        video = self.get_object()
+        user = request.user
+
+        if video.owner == user:
+            return Response({'error': 'you cant like this video'}, HTTP_400_BAD_REQUEST)
+
+        if Like.objects.filter(user=user, video=video).exists():
+            return Response({'error': 'your like are exists'}, HTTP_400_BAD_REQUEST)
+
+        Like.objects.create(user=user, video=video)
+        Video.objects.filter(id=video.id).update(total_likes=F('total_likes') + 1)
+
+        return Response({'message': 'like successfully'}, HTTP_201_CREATED)
