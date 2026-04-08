84b68b55be51e150a5c24b18dde6107587a96d51
Andrey <v.andrei.dmitrievich@gmail.com>
2026-04-08 09:20:29 +0300

    add: added admin panel

--- video_platform/video_app/admin.py
+++ video_platform/video_app/admin.py
@@ -1,3 +1,88 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner_info', 'is_published', 'name', 'total_likes', 'created_at')
+    list_select_related = ('owner', )
+    readonly_fields = ('created_at', 'owner_info_detail')
+    search_fields = ('name', 'owner__username')
+    ordering = ('-is_published', '-created_at', '-total_likes')
+
+    def owner_info(self, obj):
+        return f'{obj.owner.username}'
+    owner_info.short_description = 'Создатель'
+
+    fieldsets = (
+        (
+            'Основная информация', {
+                'fields': ('name', 'is_published')
+            }
+        ),
+        (
+            'Рейтинг и время', {
+                'fields': ('total_likes', 'created_at')
+            }
+        ),
+        (
+            'Создатель', {
+                'fields': ('owner_info_detail', )
+            }
+        )
+    )
+
+    def owner_info_detail(self, obj):
+        return f'ID: {obj.owner.id}\nUsername: {obj.owner.username}'
+    owner_info_detail.short_description = 'Создатель'
+
+
+@admin.register(models.VideoFile)
+class VideoFileAdmin(admin.ModelAdmin):
+    list_display = ('video', 'file', 'quality', 'creator')
+    list_select_related = ('video', )
+    readonly_fields = ('creator', )
+    search_fields = ('video__owner__username', 'file')
+    ordering = ('quality', )
+    fieldsets = (
+        (
+            'Видео', {
+                'fields': ('video', 'file')
+            }
+        ),
+        (
+            'Качество', {
+                'fields': ('quality', )
+            }
+        ),
+        (
+            'Создатель', {
+                'fields': ('creator', )
+            }
+        )
+    )
+
+    def creator(self, obj):
+        return f'{obj.video.owner.username}'
+    creator.short_description = 'Создатель'
+
+
+@admin.register(models.Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('video', 'user')
+    list_select_related = ('video', 'user')
+    readonly_fields = ('video', 'user')
+    search_fields = ('video__name', 'user__username')
+    fieldsets = (
+        (
+            'Видео', {
+                'fields': ('video', )
+            }
+        ),
+        (
+            'Создатель', {
+                'fields': ('user', )
+            }
+        )
+    )
