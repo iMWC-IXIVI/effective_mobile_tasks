94038a507b89960908f1712acf2fc36ebca5fb4a
Andrey <v.andrei.dmitrievich@gmail.com>
2026-04-17 23:46:53 +0300

    add: downloader copy

---
+++ async_task/sync_app/downloader/download_files.py
@@ -0,0 +1,13 @@
+import requests
+
+from core import settings
+
+
+def download_file(link: str, file_name: str) -> None:
+    """Загрузка файла из сайта"""
+
+    with requests.get(link) as response:
+        response.raise_for_status()
+        with open(settings.DOWNLOAD_DIR/file_name, 'wb') as file:
+            for chunk in response.iter_content(1024):
+                file.write(chunk)
