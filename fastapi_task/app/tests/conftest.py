df7d5391d35125aa664ef78f78d354261a0198fc
Andrey <v.andrei.dmitrievich@gmail.com>
2026-04-28 00:48:19 +0300

    add: test dates

--- fastapi_task/app/tests/conftest.py
+++ fastapi_task/app/tests/conftest.py
@@ -2,14 +2,32 @@ import pytest_asyncio
 
 from httpx import ASGITransport, AsyncClient
 
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+from asgi_lifespan import LifespanManager
+
 from main import app
+from core import settings
+
+
+@pytest_asyncio.fixture
+async def get_session():
+    """Получение бд сессии"""
+
+    engine = create_async_engine(settings.DATABASE_URL)
+    async_session = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+    async with async_session() as session:
+        yield session
 
 
 @pytest_asyncio.fixture
 async def client():
     """Асинхронный клиент для тестирования"""
 
-    transport = ASGITransport(app=app)
+    async with LifespanManager(app) as manager:
 
-    async with AsyncClient(transport=transport, base_url='http://localhost:8000') as client:
-        yield client
+        transport = ASGITransport(app=app)
+
+        async with AsyncClient(transport=transport, base_url='http://localhost:8000') as client:
+            yield client
---
+++ fastapi_task/app/tests/test_dates.py
@@ -0,0 +1,89 @@
+import time
+import asyncio
+import httpx
+import pytest
+
+from database.crud import get_db_dates
+from database.schemas import DateSchema
+
+
+base_url = '/api/v1/trading/dates'
+
+
+@pytest.mark.asyncio
+async def test_get_dates(client: httpx.AsyncClient) -> None:
+    """Тестирование /api/v1/trading/dates"""
+
+    response = await client.get(base_url)
+    data = response.json()
+
+    assert response.status_code == 200
+    assert isinstance(data, list)
+    assert len(data) <= 30
+
+
+@pytest.mark.asyncio
+async def test_get_dates_broken(client: httpx.AsyncClient) -> None:
+    """Тестирование put, post, patch, delete"""
+
+    responses = await asyncio.gather(*[
+        client.post(base_url),
+        client.put(base_url),
+        client.patch(base_url),
+        client.delete(base_url)
+    ])
+    responses_status = [response.status_code for response in responses]
+    responses_data = [response.json() for response in responses]
+
+    assert responses_status == [405, 405, 405, 405]
+    assert responses_data == [{'detail': 'Method Not Allowed'} for _ in range(4)]
+
+
+@pytest.mark.asyncio
+async def test_get_dates_last_days(client: httpx.AsyncClient) -> None:
+    """Тестирование с флагом last_days"""
+
+    response1 = await client.get(base_url + '?last_days=150')
+    data1 = response1.json()
+
+    response2 = await client.get(base_url + '?last_days=-50')
+    data2 = response2.json()
+
+    assert response1.status_code == 200
+    assert isinstance(data1, list)
+    assert len(data1) <= 150
+
+    assert response2.status_code == 200
+    assert isinstance(data2, list)
+    assert len(data2) == 0
+
+
+@pytest.mark.asyncio
+async def test_get_dates_cache(client: httpx.AsyncClient) -> None:
+    """Тестирование на сохранение в кэш"""
+
+    start = time.perf_counter()
+    response1 = await client.get(base_url + '?last_days=5')
+    first_time = time.perf_counter() - start
+
+    start = time.perf_counter()
+    response2 = await client.get(base_url + '?last_days=5')
+    second_time = time.perf_counter() - start
+
+    assert response1.status_code == 200 and response2.status_code == 200
+    assert response1.json() == response2.json()
+    assert second_time <= first_time
+
+
+@pytest.mark.asyncio
+async def test_get_db_dates(get_session) -> None:
+    """Получение данных из бд напрямую"""
+
+    last_days = 30
+    result = await get_db_dates(last_days, get_session)
+
+    for data in result:
+        assert isinstance(data, DateSchema)
+        assert hasattr(data, 'date')
+
+    assert len(result) <= last_days
